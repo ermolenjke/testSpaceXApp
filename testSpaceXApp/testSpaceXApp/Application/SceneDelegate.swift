cff355127049cabb24ef3f155eaa86b6748ccc65
ermolenjke <ermolenko.daniil.al@gmail.com>
2022-08-10 13:01:22 +0300

    commit

---
+++ testSpaceXApp/testSpaceXApp/Application/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  testSpaceXApp
+//
+//  Created by Даниил Ермоленко on 10.08.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+           
+            guard let windowScene = (scene as? UIWindowScene) else { return }
+            window = UIWindow(windowScene: windowScene)
+            window?.rootViewController =  ViewController()
+            window?.makeKeyAndVisible()
+            window?.overrideUserInterfaceStyle = .light
+        }
+
+}
+
