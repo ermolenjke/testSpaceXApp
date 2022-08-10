1a1dd3a1a9394e1d25004604c5b4b6870cfdc2e7
ermolenjke <ermolenko.daniil.al@gmail.com>
2022-08-10 12:26:56 +0300

    Initial

---
+++ testSpaceXApp/testSpaceXApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testSpaceXApp
+//
+//  Created by Даниил Ермоленко on 10.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
